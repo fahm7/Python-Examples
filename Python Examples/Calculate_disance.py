8fbb476a45cf59eed40886485d924b0a4bcdc8cb
Faheem SA <fahm.py@gmail.com>
2018-03-30 09:58:50 +0530

    python example

---
+++ Python Examples/Calculate_disance.py
@@ -0,0 +1,18 @@
+# Write a procedure, speed_fraction, which takes as its inputs the result of
+# a traceroute (in ms) and distance (in km) between two points. It should
+# return the speed the data travels as a decimal fraction of the speed of
+# light.
+
+speed_of_light = 300000. # km per second
+
+def speed_fraction(traceroute,distance):
+    speed= distance/traceroute*1000*2
+    return speed/speed_of_light
+
+
+
+print speed_fraction(50.0,5000.0)
+#>>> 0.666666666667
+
+print speed_fraction(50.0,10000.0)
+#>>> 1.33333333333  # Any thoughts about this answer, or these inputs?
