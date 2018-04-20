a8c89be85eacdc47b3a3cb561d6b45c40bbebb3d
Faheem SA <fahm.py@gmail.com>
2018-04-20 16:24:09 +0530

    ORM SQLALCHEMY
    Example

---
+++ Python Examples/orm/Python_SQL_alchemy_select.py
@@ -0,0 +1,29 @@
+
+from Python_Sql_alchemy import Person, Base, Address
+from sqlalchemy import create_engine
+
+engine = create_engine('sqlite:///sqlalchemy_example.db')
+Base.metadata.bind = engine
+
+from sqlalchemy.orm import sessionmaker
+DBSession = sessionmaker()
+DBSession.bind = engine
+session = DBSession()
+# Make a query to find all Persons in the database
+print (session.query(Person).all())
+#[<sqlalchemy_declarative.Person object at 0x2ee3a10>]
+
+# Return the first Person from all Persons in the database
+person = session.query(Person).first()
+print (person.name)
+#u'new person'
+# Find all Address whose person field is pointing to the person object
+print(session.query(Address).filter(Address.person == person).all())
+#[<sqlalchemy_declarative.Address object at 0x2ee3cd0>]
+
+# Retrieve one Address whose person field is point to the person object
+session.query(Address).filter(Address.person == person).one()
+#<sqlalchemy_declarative.Address object at 0x2ee3cd0>
+address = session.query(Address).filter(Address.person == person).one()
+print (address.post_code)
+#u'00000'
