f9b0ba283f450c53024f823a26c7de9aa17f0dd2
Faheem <fahm.py@gmail.com>
2019-06-02 07:21:11 +0530

    strings

---
+++ 01Python Basics Course01/examples/2DataTypes.py
@@ -0,0 +1,31 @@
+#PYTHON CONSTANTS
+print('________________________CONSTANTS_________________________')
+print (123)
+print (98.6 )
+print ('Hello world')
+
+print('________________________VARIABLES_________________________')
+
+x=12.5
+y='s'
+z=55
+
+print('________________________NUMERIC EXPRESSIONS_________________________')
+xx=2
+xx=xx+2
+print(xx)
+
+yy = 440 * 12
+print(yy)
+
+zz = yy / 1000
+print(zz)
+
+jj = 23
+kk = jj % 5
+print (kk )
+print (4 ** 3 )
+
+x = 1 + 2 ** 3 / 4 * 5
+print(x)
+
---
+++ 01Python Basics Course01/examples/3StringsEx.py
@@ -0,0 +1,35 @@
+
+print('________________________STRINGS_________________________')
+
+s = "I am a string"
+print(s)
+
+mys = 'abcdefghijklmnopqrstuvwxyz'
+print(mys)
+
+print(type(mys))
+
+name = 'Dave'
+print ('Hello ' + name + '!' + '!' + '!')
+
+
+mys = 'abcdefghijklmnopqrstuvwxyz'
+print(mys[0] )
+
+print(mys[1+2])
+
+name = 'Fahm'
+print (name[-1])
+
+word = 'assume'
+print (word[3])
+
+print('________________________STRINGS FUNCTIONSS_________________________')
+print ('{} {} {}'.format('a', 'b', 'c'))
+
+# Numbered fields refer to the position of the arguments
+print('{2} {1} {0}'.format('a', 'b', 'c') )
+
+log_message = "IP address {} accessed {} at {}".format(user_ip, url, now)
+print (log_message)
+
